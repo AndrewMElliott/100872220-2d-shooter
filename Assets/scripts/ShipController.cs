165aceca68723f212631d992b6f1f5377499d500
AndrewMElliott <32204180+AndrewMElliott@users.noreply.github.com>
2017-10-16 01:51:28 -0400

    added ship movement

---
+++ Assets/scripts/ShipController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+	[SerializeField]
+	private float shipSpeed = 5f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveShip ();
+	}
+
+	private void MoveShip ()
+	{
+
+		if (Input.GetKey (KeyCode.W)) {
+			Vector2 position = this.transform.position;
+			position.y += shipSpeed;
+			if (position.y > 4.6) {
+
+			} else {
+				this.transform.position = position;
+			}
+				
+
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			Vector2 position = this.transform.position;
+			position.y -= shipSpeed;
+			if (position.y < -4.6) {
+
+			} else {
+				this.transform.position = position;
+			}
+
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			Vector2 position = this.transform.position;
+			position.x -= shipSpeed;
+			if(position.x < -7.2)
+			{
+
+			}else{
+				this.transform.position = position;
+			}
+
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			Vector2 position = this.transform.position;
+			position.x += shipSpeed;
+			if(position.x > 6.5)
+			{
+
+			}else{
+				this.transform.position = position;
+			}
+
+		}
+	}
+}
